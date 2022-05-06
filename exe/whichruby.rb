11676fbd8d647490d8a860fca697dcadc33bdc63
ccmywish <ccmywish@qq.com>
2022-05-06 14:45:51 +0800

    Add exe

---
+++ exe/whichruby.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# ---------------------------------------------------------------
+# File          : whichruby.rb
+# Authors       : ccmywish <ccmywish@qq.com>
+# Created on    : <2022-05-06>
+# Last modified : <2022-05-06>
+#
+# whichruby:
+#
+#   Test which ruby is called using this gem.
+#
+# ---------------------------------------------------------------
+
+puts RUBY_DESCRIPTION
